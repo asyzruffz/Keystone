8fccb105657affb1cc4f8e72945f3fb3d680b39b
Asyraf <asyzruffz@gmail.com>
2026-07-27 09:36:42 +0800

    Add Sender & DomainEventsDispatcher

---
+++ Application/DomainEventsDispatcher.cs
@@ -0,0 +1,68 @@
+﻿using Keystone.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Concurrent;
+
+namespace Keystone.Application;
+
+internal sealed class DomainEventsDispatcher : IDomainEventsDispatcher
+{
+    static readonly ConcurrentDictionary<Type, Type> handlerTypeDictionary = new();
+    static readonly ConcurrentDictionary<Type, Type> wrapperTypeDictionary = new();
+
+    readonly IServiceProvider provider;
+
+    public DomainEventsDispatcher(IServiceProvider serviceProvider)
+    {
+        provider = serviceProvider;
+    }
+
+    public async ValueTask DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken ct = default)
+    {
+        foreach (IDomainEvent domainEvent in domainEvents)
+        {
+            using var scope = provider.CreateScope();
+
+            Type domainEventType = domainEvent.GetType();
+            Type handlerType = handlerTypeDictionary.GetOrAdd(domainEventType,
+                et => typeof(IDomainEventHandler<>).MakeGenericType(et));
+
+            IEnumerable<object?> handlers = scope.ServiceProvider.GetServices(handlerType);
+
+            foreach (object? handler in handlers)
+            {
+                if (handler is null) continue;
+
+                var handlerWrapper = HandlerWrapper.Create(handler, domainEventType);
+
+                await handlerWrapper.Handle(domainEvent, ct);
+            }
+        }
+    }
+
+    private abstract class HandlerWrapper
+    {
+        public abstract ValueTask Handle(IDomainEvent domainEvent, CancellationToken ct);
+
+        public static HandlerWrapper Create(object handler, Type domainEventType)
+        {
+            Type wrapperType = wrapperTypeDictionary.GetOrAdd(domainEventType,
+                et => typeof(HandlerWrapper<>).MakeGenericType(et));
+            return (HandlerWrapper)Activator.CreateInstance(wrapperType, handler)!;
+        }
+    }
+
+    private sealed class HandlerWrapper<TDomainEvent> : HandlerWrapper where TDomainEvent : IDomainEvent
+    {
+        private readonly IDomainEventHandler<TDomainEvent> handler;
+
+        public HandlerWrapper(object handler)
+        {
+            this.handler = (IDomainEventHandler<TDomainEvent>)handler;
+        }
+
+        public override ValueTask Handle(IDomainEvent domainEvent, CancellationToken ct)
+        {
+            return handler.Handle((TDomainEvent)domainEvent, ct);
+        }
+    }
+}
---
+++ Application/Sender.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Concurrent;
+
+namespace Keystone.Application;
+
+internal class Sender : ISender
+{
+    static readonly ConcurrentDictionary<Type, HandlerWrapperBase> handlerDictionary = new();
+
+    readonly IServiceProvider provider;
+
+    public Sender(IServiceProvider serviceProvider)
+    {
+        provider = serviceProvider;
+    }
+
+    public ValueTask<Result> Send<TOperation>(TOperation operation, CancellationToken ct = default)
+        where TOperation : IOperation
+    {
+        var handler = provider.GetRequiredService<IOperationHandler<TOperation>>();
+        return handler.Handle(operation, ct);
+    }
+
+    public async ValueTask<Result<TResponse>> Send<TResponse>(IOperation<TResponse> operation, CancellationToken ct = default)
+    {
+        Type operationType = operation.GetType();
+        Type responseType = typeof(TResponse);
+        var handler = handlerDictionary.GetOrAdd(operationType, ot =>
+        {
+            var wrapperType = typeof(HandlerWrapper<,>).MakeGenericType(ot, responseType);
+            var wrapper = Activator.CreateInstance(wrapperType) ?? throw new InvalidOperationException($"Could not create wrapper type for {ot}");
+            return (HandlerWrapperBase)wrapper;
+        });
+
+        return (Result<TResponse>)(await handler.Handle(operation, provider, ct).ConfigureAwait(false))!;
+    }
+
+    private abstract class HandlerWrapperBase
+    {
+        public abstract ValueTask<object?> Handle(object operation, IServiceProvider provider,
+            CancellationToken ct);
+    }
+
+    private class HandlerWrapper<TOperation, TResponse> : HandlerWrapperBase
+        where TOperation : IOperation<TResponse>
+    {
+        public override async ValueTask<object?> Handle(object operation, IServiceProvider provider,
+            CancellationToken ct)
+        {
+            return await Handle((IOperation<TResponse>)operation, provider, ct).ConfigureAwait(false);
+        }
+
+        public ValueTask<Result<TResponse>> Handle(IOperation<TResponse> operation, IServiceProvider provider,
+            CancellationToken ct)
+        {
+            var handler = provider.GetRequiredService<IOperationHandler<TOperation, TResponse>>();
+            return handler.Handle((TOperation)operation, ct);
+        }
+    }
+}
