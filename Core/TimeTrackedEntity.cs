8eb8047ce071d9ac293ddd2d195f66714568b020
Asyraf <asyzruffz@gmail.com>
2026-07-27 10:59:37 +0800

    Add TimeTrackedEntity

---
+++ Core/TimeTrackedEntity.cs
@@ -0,0 +1,30 @@
+﻿namespace Keystone.Core;
+
+public abstract class TimeTrackedEntity
+{
+    public DateTime Created { get; private set; }
+    public DateTime Modified { get; private set; }
+    public DateTime LastActive { get; private set; }
+
+    public TimeTrackedEntity()
+    {
+        CreatedNow();
+    }
+
+    protected void CreatedNow()
+    {
+        Created = DateTime.UtcNow;
+        Modified = DateTime.UtcNow;
+        LastActive = DateTime.UtcNow;
+    }
+
+    public void ModifiedNow()
+    {
+        Modified = DateTime.UtcNow;
+    }
+
+    public void ActiveNow()
+    {
+        LastActive = DateTime.UtcNow;
+    }
+}
