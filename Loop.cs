0a83270c53aafcfa89d00253581249a20cdd6413
Asyraf <asyzruffz@gmail.com>
2026-07-27 09:00:53 +0800

    Add Result, Option, Loop, and Throw

---
+++ Loop.cs
@@ -0,0 +1,16 @@
+﻿namespace Keystone;
+
+public static class Loop
+{
+    public static void For(int size, Action<int> action)
+    {
+        for (int i = 0; i < size; i++)
+            action?.Invoke(i);
+    }
+
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+    {
+        foreach (T item in enumerable) action?.Invoke(item);
+        return enumerable;
+    }
+}
---
+++ Throw.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace Keystone;
+
+public static class Throw
+{
+    public static void Unreachable() => throw new UnreachableException();
+    public static T Unreachable<T>() => throw new UnreachableException();
+
+    public static void InvalidOperation(string message) => throw new InvalidOperationException(message);
+    public static T InvalidOperation<T>(string message) => throw new InvalidOperationException(message);
+
+    public static void InvalidArgument(string message) => throw new ArgumentException(message);
+    public static T InvalidArgument<T>(string message) => throw new ArgumentException(message);
+}
+
