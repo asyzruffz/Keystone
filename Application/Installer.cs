15db4b96168974268b5b3f64baa2685503c534f1
Asyraf <asyzruffz@gmail.com>
2026-07-27 10:28:49 +0800

    Add application installer

---
+++ Application/Installer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+using System.Reflection;
+
+namespace Keystone.Application;
+
+public static class Installer
+{
+    public static IServiceCollection AddKeystoneApplication(this IServiceCollection services)
+    {
+        services.AddScoped<ISender, Sender>(sp => new Sender(sp));
+        services.AddTransient<IDomainEventsDispatcher, DomainEventsDispatcher>();
+        return services;
+    }
+
+    public static IServiceCollection RegisterOperationHandlers(this IServiceCollection services,
+        params Assembly[] assemblies)
+    {
+        services.Scan(scan => scan
+            .FromAssemblies(assemblies)
+            .AddClasses(classes => classes.AssignableTo(typeof(IOperationHandler<>)), publicOnly: false)
+            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(IOperationHandler<,>)), publicOnly: false)
+            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+        return services;
+    }
+
+    public static IServiceCollection RegisterDomainEventHandlers(this IServiceCollection services,
+        params Assembly[] assemblies)
+    {
+        services.Scan(scan => scan
+            .FromAssemblies(assemblies)
+            .AddClasses(classes => classes.AssignableTo(typeof(IDomainEventHandler<>)), publicOnly: false)
+            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+        return services;
+    }
+}
