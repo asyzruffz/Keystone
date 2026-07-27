a45c054d3268340f62a63c45c238485de2774ebd
Asyraf <asyzruffz@gmail.com>
2026-07-27 10:51:38 +0800

    Add repository interface

---
+++ Core/IQueryableById.cs
@@ -0,0 +1,13 @@
+﻿namespace Keystone.Core;
+
+public interface IQueryableById<T, TId>
+{
+    Option<T> GetById(TId id);
+    bool HasWithId(TId id);
+}
+
+public interface IQueryableById<T, TId1, TId2>
+{
+    Option<T> GetById(TId1 id1, TId2 id2);
+    bool HasWithId(TId1 id1, TId2 id2);
+}
---
+++ Core/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Keystone.Core;
+
+public interface IRepository<T> where T : class
+{
+    IQueryable<T> GetAll();
+    void LoadSingle<TResult>(T obj, Expression<Func<T, TResult?>> expression) where TResult : class;
+    void LoadCollection<TResult>(T obj, Expression<Func<T, IEnumerable<TResult>>> expression) where TResult : class;
+    void Create(T obj);
+    void Create(IEnumerable<T> objs);
+    void Update(T obj);
+    void Delete(T obj);
+    void Attach(T obj);
+    void Attach(IEnumerable<T> objs);
+}
