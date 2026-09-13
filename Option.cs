e3906ba193ff8ed36dc246532e323ab7900d6f2c
Asyraf <asyzruffz@gmail.com>
2026-09-13 20:30:51 +0800

    Update async support for option and result

--- Option.cs
+++ Option.cs
@@ -1,6 +1,6 @@
 ﻿namespace Keystone;
 
-public abstract class Option<T>
+public abstract record Option<T>
 {
     public static Option<T> Some(T data) => Some<T>.Of(data);
     public static Option<T> None() => None<T>.Existed();
@@ -12,9 +12,17 @@ public abstract class Option<T>
     public abstract bool IsNone { get; }
     public bool IsSome => !IsNone;
 
-    public abstract void DoWith(Action<T> onSome);
+    public abstract void DoWith(Action<T> action);
+    public abstract ValueTask DoWithAsync(Func<T, CancellationToken, ValueTask> action, CancellationToken ct);
+
     public abstract Option<TResult> Map<TResult>(Func<T, TResult> map);
+    public abstract ValueTask<Option<TResult>> MapAsync<TResult>(
+        Func<T, CancellationToken, ValueTask<TResult>> map, CancellationToken ct);
+
     public abstract Option<TResult> Then<TResult>(Func<T, Option<TResult>> action);
+    public abstract ValueTask<Option<TResult>> ThenAsync<TResult>(
+        Func<T, CancellationToken, ValueTask<Option<TResult>>> action, CancellationToken ct);
+
     public void Match(Action<T> onSome, Action onNone)
     {
         switch (this)
@@ -31,16 +39,27 @@ public abstract class Option<T>
         _ => Throw.Unreachable<TResult>(),
     };
 
-    #region Equality
-    public static bool operator ==(Option<T>? a, Option<T>? b) => a is null ? b is null : a.Equals(b);
-    public static bool operator !=(Option<T>? a, Option<T>? b) => !(a == b);
-    public override bool Equals(object? other) => other is Option<T> &&
-        Match(onSome: _ => Equals(other as Some<T>), onNone: () => Equals(other as None<T>));
-    public override int GetHashCode() => base.GetHashCode();
-    #endregion
+    public ValueTask MatchAsync(
+        Func<T, CancellationToken, ValueTask> onSome,
+        Func<CancellationToken, ValueTask> onNone,
+        CancellationToken ct) => this switch
+        {
+            Some<T> some => onSome(some.Data, ct),
+            None<T> => onNone(ct),
+            _ => Throw.Unreachable<ValueTask>(),
+        };
+    public ValueTask<TResult> MatchAsync<TResult>(
+        Func<T, CancellationToken, ValueTask<TResult>> onSome,
+        Func<CancellationToken, ValueTask<TResult>> onNone,
+        CancellationToken ct) => this switch
+        {
+            Some<T> some => onSome(some.Data, ct),
+            None<T> => onNone(ct),
+            _ => Throw.Unreachable<ValueTask<TResult>>(),
+        };
 }
 
-public class Some<T> : Option<T>, IEquatable<Some<T>>
+public sealed record Some<T> : Option<T>, IEquatable<Some<T>>
 {
     private T data;
 
@@ -52,21 +71,24 @@ public class Some<T> : Option<T>, IEquatable<Some<T>>
 
     public override bool IsNone => false;
 
-    public override void DoWith(Action<T> onSome) => onSome(data);
-    public override Option<TResult> Map<TResult>(Func<T, TResult> map) => Option<TResult>.Some(map(data));
-    public override Option<TResult> Then<TResult>(Func<T, Option<TResult>> action) => action(data);
-    public override string ToString() => $"Some({data})";
+    public override void DoWith(Action<T> action) => action(data);
+    public override ValueTask DoWithAsync(Func<T, CancellationToken, ValueTask> action, CancellationToken ct) =>
+        action(data, ct);
 
-    #region Equality
-    public static bool operator ==(Some<T>? a, Some<T>? b) => a is null ? b is null : a.Equals(b);
-    public static bool operator !=(Some<T>? a, Some<T>? b) => !(a == b);
-    public override bool Equals(object? other) => other is Some<T> && Equals(other as Some<T>);
-    public bool Equals(Some<T>? other) => other is not null && EqualityComparer<T>.Default.Equals(data, other.data);
-    public override int GetHashCode() => data!.GetHashCode();
-    #endregion
+    public override Option<TResult> Map<TResult>(Func<T, TResult> map) => Option<TResult>.Some(map(data));
+    public override async ValueTask<Option<TResult>> MapAsync<TResult>(
+        Func<T, CancellationToken, ValueTask<TResult>> map, CancellationToken ct) =>
+        Option<TResult>.Some(await map(data, ct).ConfigureAwait(false));
+
+    public override Option<TResult> Then<TResult>(Func<T, Option<TResult>> action) => action(data);
+    public override ValueTask<Option<TResult>> ThenAsync<TResult>(
+        Func<T, CancellationToken, ValueTask<Option<TResult>>> action, CancellationToken ct) =>
+        action(data, ct);
+
+    public override string ToString() => $"Some({data})";
 }
 
-public class None<T> : Option<T>, IEquatable<None<T>>
+public sealed record None<T> : Option<T>, IEquatable<None<T>>
 {
     static None<T> value = new None<T>();
 
@@ -77,24 +99,73 @@ public class None<T> : Option<T>, IEquatable<None<T>>
     public override T Or(T defaultVal) => defaultVal;
 
     public override bool IsNone => true;
-    
-    public override void DoWith(Action<T> onSome) { }
-    public override Option<TResult> Map<TResult>(Func<T, TResult> map) => Option<TResult>.None();
-    public override Option<TResult> Then<TResult>(Func<T, Option<TResult>> action) => Option<TResult>.None();
-    public override string ToString() => $"None<{typeof(T).Name}>";
 
-    #region Equality
-    public static bool operator ==(None<T>? a, None<T>? b) => a is null ? b is null : a.Equals(b);
-    public static bool operator !=(None<T>? a, None<T>? b) => !(a == b);
-    public override bool Equals(object? other) => other is None<T> && Equals(other as None<T>);
-    public bool Equals(None<T>? other) => other is not null;
-    public override int GetHashCode() => 0;
-    #endregion
+    public override void DoWith(Action<T> onSome) { }
+    public override ValueTask DoWithAsync(Func<T, CancellationToken, ValueTask> onSome, CancellationToken ct) =>
+        ValueTask.CompletedTask;
+
+    public override Option<TResult> Map<TResult>(Func<T, TResult> map) => Option<TResult>.None();
+    public override ValueTask<Option<TResult>> MapAsync<TResult>(
+        Func<T, CancellationToken, ValueTask<TResult>> map, CancellationToken ct) =>
+        ValueTask.FromResult(Option<TResult>.None());
+
+    public override Option<TResult> Then<TResult>(Func<T, Option<TResult>> action) => Option<TResult>.None();
+    public override ValueTask<Option<TResult>> ThenAsync<TResult>(
+        Func<T, CancellationToken, ValueTask<Option<TResult>>> action, CancellationToken ct) =>
+        ValueTask.FromResult(Option<TResult>.None());
+
+    public override string ToString() => $"None<{typeof(T).Name}>";
 }
 
 public static class OptionExtensions
 {
     public static Option<T> AsOption<T>(this T? data) => Option<T>.From(data);
-    public static Result<T> ToResult<T>(this Option<T> option, string errorMessage = "") => 
+    public static Result<T> ToResult<T>(this Option<T> option, string errorMessage = "") =>
         option.Match(Result<T>.Ok, () => Result<T>.Fail(errorMessage));
+
+    // ----------------
+
+    public static async ValueTask DoWith<T>(this ValueTask<Option<T>> option,
+        Action<T> action) =>
+        (await option.ConfigureAwait(false)).DoWith(action);
+    public static async ValueTask DoWithAsync<T>(this ValueTask<Option<T>> option,
+        Func<T, CancellationToken, ValueTask> action,
+        CancellationToken ct) =>
+        await (await option.ConfigureAwait(false)).DoWithAsync(action, ct).ConfigureAwait(false);
+
+    public static async ValueTask<Option<TResult>> Map<T, TResult>(this ValueTask<Option<T>> option,
+        Func<T, TResult> map) =>
+        (await option.ConfigureAwait(false)).Map(map);
+    public static async ValueTask<Option<TResult>> MapAsync<T, TResult>(this ValueTask<Option<T>> option,
+        Func<T, CancellationToken, ValueTask<TResult>> map,
+        CancellationToken ct) =>
+        await (await option.ConfigureAwait(false)).MapAsync(map, ct).ConfigureAwait(false);
+
+    public static async ValueTask<Option<TResult>> Then<T, TResult>(this ValueTask<Option<T>> option,
+        Func<T, Option<TResult>> action) =>
+        (await option.ConfigureAwait(false)).Then(action);
+    public static async ValueTask<Option<TResult>> ThenAsync<T, TResult>(this ValueTask<Option<T>> option,
+        Func<T, CancellationToken, ValueTask<Option<TResult>>> action,
+        CancellationToken ct) =>
+        await (await option.ConfigureAwait(false)).ThenAsync(action, ct).ConfigureAwait(false);
+
+    public static async ValueTask Match<T>(this ValueTask<Option<T>> option,
+        Action<T> onSome, Action onNone) =>
+        (await option.ConfigureAwait(false)).Match(onSome, onNone);
+    public static async ValueTask<TResult> Match<T, TResult>(this ValueTask<Option<T>> option,
+        Func<T, TResult> onSome, Func<TResult> onNone) =>
+        (await option.ConfigureAwait(false)).Match(onSome, onNone);
+
+    public static async ValueTask MatchAsync<T>(this ValueTask<Option<T>> option,
+        Func<T, CancellationToken, ValueTask> onSome,
+        Func<CancellationToken, ValueTask> onNone,
+        CancellationToken ct) =>
+        await (await option.ConfigureAwait(false)).MatchAsync(onSome, onNone, ct)
+            .ConfigureAwait(false);
+    public static async ValueTask<TResult> MatchAsync<T, TResult>(this ValueTask<Option<T>> option,
+        Func<T, CancellationToken, ValueTask<TResult>> onSome,
+        Func<CancellationToken, ValueTask<TResult>> onNone,
+        CancellationToken ct) =>
+        await (await option.ConfigureAwait(false)).MatchAsync(onSome, onNone, ct)
+            .ConfigureAwait(false);
 }
--- Result.cs
+++ Result.cs
@@ -1,6 +1,6 @@
 ﻿namespace Keystone;
 
-public class Result
+public record Result
 {
     public bool IsSuccess { get; init; }
     public string ErrorMessage { get; init; }
@@ -18,42 +18,45 @@ public class Result
     public Result FixError(Func<string, Result> action) =>
         !IsSuccess ? action(ErrorMessage) : this;
 
+    public void Then(Action action) { if (IsSuccess) action(); }
     public Result Then(Func<Result> action) =>
         IsSuccess ? action() : this;
     public Result<TResult> Then<TResult>(Func<Result<TResult>> action) =>
         IsSuccess ? action() : Result<TResult>.Fail(ErrorMessage);
-    public void Then(Action action) { if (IsSuccess) action(); }
 
-    public Task<Result> ThenAsync(Func<CancellationToken, Task<Result>> action, CancellationToken ct = default) =>
-        IsSuccess ? action(ct) : Task.FromResult(this);
-    public Task<Result<TResult>> ThenAsync<TResult>(Func<CancellationToken, Task<Result<TResult>>> action, CancellationToken ct = default) =>
-        IsSuccess ? action(ct) : Task.FromResult(Result<TResult>.Fail(ErrorMessage));
-    public Task ThenAsync(Func<CancellationToken, Task> action, CancellationToken ct = default) => IsSuccess ? action(ct) : Task.CompletedTask;
+    public ValueTask ThenAsync(Func<CancellationToken, ValueTask> action, CancellationToken ct = default) =>
+        IsSuccess ? action(ct) : ValueTask.CompletedTask;
+    public ValueTask<Result> ThenAsync(Func<CancellationToken, ValueTask<Result>> action, CancellationToken ct = default) =>
+        IsSuccess ? action(ct) : ValueTask.FromResult(this);
+    public ValueTask<Result<TResult>> ThenAsync<TResult>(Func<CancellationToken, ValueTask<Result<TResult>>> action, CancellationToken ct = default) =>
+        IsSuccess ? action(ct) : ValueTask.FromResult(Result<TResult>.Fail(ErrorMessage));
 
     public Result<TResult> Map<TResult>(Func<TResult> action) =>
         Then(() => Result<TResult>.Ok(action()));
 
-    public Task<Result<TResult>> MapAsync<TResult>(
-        Func<CancellationToken, Task<TResult>> action,
+    public ValueTask<Result<TResult>> MapAsync<TResult>(
+        Func<CancellationToken, ValueTask<TResult>> action,
         CancellationToken ct = default) =>
-        ThenAsync(async ct => Result<TResult>.Ok(await action(ct)), ct);
+        ThenAsync(async ct => Result<TResult>.Ok(await action(ct).ConfigureAwait(false)), ct);
 
+    public void Match(Action onSuccess, Action<string> onFailure)
+    { if (IsSuccess) onSuccess(); else onFailure(ErrorMessage); }
     public TResult Match<TResult>(Func<TResult> onSuccess, Func<string, TResult> onFailure) =>
         IsSuccess ? onSuccess() : onFailure(ErrorMessage);
 
-    public Task MatchAsync(
-        Func<Task> onSuccess,
-        Func<string, Task> onFailure,
+    public ValueTask MatchAsync(
+        Func<ValueTask> onSuccess,
+        Func<string, ValueTask> onFailure,
         CancellationToken ct = default) =>
         IsSuccess ? onSuccess() : onFailure(ErrorMessage);
-    public Task<TResult> MatchAsync<TResult>(
-        Func<Task<TResult>> onSuccess,
-        Func<string, Task<TResult>> onFailure,
+    public ValueTask<TResult> MatchAsync<TResult>(
+        Func<ValueTask<TResult>> onSuccess,
+        Func<string, ValueTask<TResult>> onFailure,
         CancellationToken ct = default) =>
         IsSuccess ? onSuccess() : onFailure(ErrorMessage);
 }
 
-public class Result<T>
+public record Result<T>
 {
     public bool IsSuccess { get; init; }
     public string ErrorMessage { get; init; }
@@ -70,43 +73,47 @@ public class Result<T>
     public static Result<T> Ok(T value) => new Result<T>(true, value, string.Empty);
     public static Result<T> Fail(string errorMessage) => new Result<T>(false, default!, errorMessage);
 
+    public T Data => IsSuccess ? Value : Throw.InvalidOperation<T>("Invalid data in None");
     public T Or(T defaultVal) => IsSuccess ? Value : defaultVal;
 
     public Result<T> OnError(Action<string> action) { if (!IsSuccess) action(ErrorMessage); return this; }
     public Result<T> FixError(Func<string, Result<T>> action) =>
         !IsSuccess ? action(ErrorMessage) : this;
 
+    public void Then(Action<T> action) { if (IsSuccess) action(Value); }
     public Result Then(Func<T, Result> action) =>
         IsSuccess ? action(Value) : Result.Fail(ErrorMessage);
     public Result<TResult> Then<TResult>(Func<T, Result<TResult>> action) =>
         IsSuccess ? action(Value) : Result<TResult>.Fail(ErrorMessage);
-    public void Then(Action<T> action) { if (IsSuccess) action(Value); }
 
-    public Task<Result> ThenAsync(Func<T, CancellationToken, Task<Result>> action, CancellationToken ct = default) =>
-        IsSuccess ? action(Value, ct) : Task.FromResult(Result.Fail(ErrorMessage));
-    public Task<Result<TResult>> ThenAsync<TResult>(Func<T, CancellationToken, Task<Result<TResult>>> action, CancellationToken ct = default) =>
-        IsSuccess ? action(Value, ct) : Task.FromResult(Result<TResult>.Fail(ErrorMessage));
-    public Task ThenAsync(Func<T, CancellationToken, Task> action, CancellationToken ct = default) => IsSuccess ? action(Value, ct) : Task.CompletedTask;
+    public ValueTask ThenAsync(Func<T, CancellationToken, ValueTask> action, CancellationToken ct = default) =>
+        IsSuccess ? action(Value, ct) : ValueTask.CompletedTask;
+    public ValueTask<Result> ThenAsync(Func<T, CancellationToken, ValueTask<Result>> action, CancellationToken ct = default) =>
+        IsSuccess ? action(Value, ct) : ValueTask.FromResult(Result.Fail(ErrorMessage));
+    public ValueTask<Result<TResult>> ThenAsync<TResult>(Func<T, CancellationToken, ValueTask<Result<TResult>>> action, CancellationToken ct = default) =>
+        IsSuccess ? action(Value, ct) : ValueTask.FromResult(Result<TResult>.Fail(ErrorMessage));
 
     public Result<TResult> Map<TResult>(Func<T, TResult> action) =>
         Then(v => Result<TResult>.Ok(action(v)));
 
-    public Task<Result<TResult>> MapAsync<TResult>(
-        Func<T, CancellationToken, Task<TResult>> action,
+    public ValueTask<Result<TResult>> MapAsync<TResult>(
+        Func<T, CancellationToken, ValueTask<TResult>> action,
         CancellationToken ct = default) =>
-        ThenAsync(async (v, ct) => Result<TResult>.Ok(await action(v, ct)), ct);
+        ThenAsync(async (v, ct) => Result<TResult>.Ok(await action(v, ct).ConfigureAwait(false)), ct);
 
+    public void Match(Action<T> onSuccess, Action<string> onFailure)
+    { if (IsSuccess) onSuccess(Value); else onFailure(ErrorMessage); }
     public TResult Match<TResult>(Func<T, TResult> onSuccess, Func<string, TResult> onFailure) =>
         IsSuccess ? onSuccess(Value) : onFailure(ErrorMessage);
 
-    public Task MatchAsync(
-        Func<T, CancellationToken, Task> onSuccess,
-        Func<string, CancellationToken, Task> onFailure,
+    public ValueTask MatchAsync(
+        Func<T, CancellationToken, ValueTask> onSuccess,
+        Func<string, CancellationToken, ValueTask> onFailure,
         CancellationToken ct = default) =>
         IsSuccess ? onSuccess(Value, ct) : onFailure(ErrorMessage, ct);
-    public Task<TResult> MatchAsync<TResult>(
-        Func<T, CancellationToken, Task<TResult>> onSuccess,
-        Func<string, CancellationToken, Task<TResult>> onFailure,
+    public ValueTask<TResult> MatchAsync<TResult>(
+        Func<T, CancellationToken, ValueTask<TResult>> onSuccess,
+        Func<string, CancellationToken, ValueTask<TResult>> onFailure,
         CancellationToken ct = default) =>
         IsSuccess ? onSuccess(Value, ct) : onFailure(ErrorMessage, ct);
 }
@@ -115,4 +122,121 @@ public static class ResultExtensions
 {
     public static Option<T> ToOption<T>(this Result<T> result) =>
         result.Match(Option<T>.Some, _ => Option<T>.None());
+
+    // ----------------
+
+    public static async ValueTask<Result> OnError(this ValueTask<Result> result,
+        Action<string> action) =>
+        (await result.ConfigureAwait(false)).OnError(action);
+    public static async ValueTask<Result> FixError(this ValueTask<Result> result,
+        Func<string, Result> action) =>
+        (await result.ConfigureAwait(false)).FixError(action);
+
+    public static async ValueTask<Result> Then(this ValueTask<Result> result,
+        Func<Result> action) =>
+        (await result.ConfigureAwait(false)).Then(action);
+    public static async ValueTask<Result<TResult>> Then<TResult>(this ValueTask<Result> result,
+        Func<Result<TResult>> action) =>
+        (await result.ConfigureAwait(false)).Then(action);
+    public static async ValueTask Then(this ValueTask<Result> result, Action action) =>
+        (await result.ConfigureAwait(false)).Then(action);
+
+    public static async ValueTask ThenAsync(this ValueTask<Result> result,
+        Func<CancellationToken, ValueTask> action, CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).ThenAsync(action, ct).ConfigureAwait(false);
+    public static async ValueTask<Result> ThenAsync(this ValueTask<Result> result,
+        Func<CancellationToken, ValueTask<Result>> action, CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).ThenAsync(action, ct).ConfigureAwait(false);
+    public static async ValueTask<Result<TResult>> ThenAsync<TResult>(this ValueTask<Result> result,
+        Func<CancellationToken, ValueTask<Result<TResult>>> action, CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).ThenAsync(action, ct).ConfigureAwait(false);
+
+    public static async ValueTask<Result<TResult>> Map<TResult>(this ValueTask<Result> result,
+        Func<TResult> action) =>
+        (await result.ConfigureAwait(false)).Map(action);
+    public static async ValueTask<Result<TResult>> MapAsync<TResult>(this ValueTask<Result> result,
+        Func<CancellationToken, ValueTask<TResult>> action,
+        CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).MapAsync(action, ct).ConfigureAwait(false);
+
+    public static async ValueTask Match(this ValueTask<Result> result,
+        Action onSuccess, Action<string> onFailure) =>
+        (await result.ConfigureAwait(false)).Match(onSuccess, onFailure);
+    public static async ValueTask<TResult> Match<TResult>(this ValueTask<Result> result,
+        Func<TResult> onSuccess,
+        Func<string, TResult> onFailure) =>
+        (await result.ConfigureAwait(false)).Match(onSuccess, onFailure);
+
+    public static async ValueTask MatchAsync(this ValueTask<Result> result,
+        Func<ValueTask> onSuccess,
+        Func<string, ValueTask> onFailure,
+        CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).MatchAsync(onSuccess, onFailure, ct)
+            .ConfigureAwait(false);
+    public static async ValueTask<TResult> MatchAsync<TResult>(this ValueTask<Result> result,
+        Func<ValueTask<TResult>> onSuccess,
+        Func<string, ValueTask<TResult>> onFailure,
+        CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).MatchAsync(onSuccess, onFailure, ct)
+            .ConfigureAwait(false);
+
+    // ----------------
+
+    public static async ValueTask<Result<T>> OnError<T>(this ValueTask<Result<T>> result,
+        Action<string> action) =>
+        (await result.ConfigureAwait(false)).OnError(action);
+    public static async ValueTask<Result<T>> FixError<T>(this ValueTask<Result<T>> result,
+        Func<string, Result<T>> action) =>
+        (await result.ConfigureAwait(false)).FixError(action);
+
+    public static async ValueTask Then<T>(this ValueTask<Result<T>> result,
+        Action<T> action) =>
+        (await result.ConfigureAwait(false)).Then(action);
+    public static async ValueTask<Result> Then<T>(this ValueTask<Result<T>> result,
+        Func<T, Result> action) =>
+        (await result.ConfigureAwait(false)).Then(action);
+    public static async ValueTask<Result<TResult>> Then<T, TResult>(this ValueTask<Result<T>> result,
+        Func<T, Result<TResult>> action) =>
+        (await result.ConfigureAwait(false)).Then(action);
+
+    public static async ValueTask ThenAsync<T>(this ValueTask<Result<T>> result,
+        Func<T, CancellationToken, ValueTask> action,
+        CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).ThenAsync(action, ct).ConfigureAwait(false);
+    public static async ValueTask<Result> ThenAsync<T>(this ValueTask<Result<T>> result,
+        Func<T, CancellationToken, ValueTask<Result>> action,
+        CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).ThenAsync(action, ct).ConfigureAwait(false);
+    public static async ValueTask<Result<TResult>> ThenAsync<T, TResult>(this ValueTask<Result<T>> result,
+        Func<T, CancellationToken, ValueTask<Result<TResult>>> action,
+        CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).ThenAsync(action, ct).ConfigureAwait(false);
+
+    public static async ValueTask<Result<TResult>> Map<T, TResult>(this ValueTask<Result<T>> result,
+        Func<T, TResult> action) =>
+        (await result.ConfigureAwait(false)).Map(action);
+    public static async ValueTask<Result<TResult>> MapAsync<T, TResult>(this ValueTask<Result<T>> result,
+        Func<T, CancellationToken, ValueTask<TResult>> action,
+        CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).MapAsync(action, ct).ConfigureAwait(false);
+
+    public static async ValueTask Match<T>(this ValueTask<Result<T>> result,
+        Action<T> onSuccess, Action<string> onFailure) =>
+        (await result.ConfigureAwait(false)).Match(onSuccess, onFailure);
+    public static async ValueTask<TResult> Match<T, TResult>(this ValueTask<Result<T>> result,
+        Func<T, TResult> onSuccess, Func<string, TResult> onFailure) =>
+        (await result.ConfigureAwait(false)).Match(onSuccess, onFailure);
+
+    public static async ValueTask MatchAsync<T>(this ValueTask<Result<T>> result,
+        Func<T, CancellationToken, ValueTask> onSuccess,
+        Func<string, CancellationToken, ValueTask> onFailure,
+        CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).MatchAsync(onSuccess, onFailure, ct)
+            .ConfigureAwait(false);
+    public static async ValueTask<TResult> MatchAsync<T, TResult>(this ValueTask<Result<T>> result,
+        Func<T, CancellationToken, ValueTask<TResult>> onSuccess,
+        Func<string, CancellationToken, ValueTask<TResult>> onFailure,
+        CancellationToken ct = default) =>
+        await (await result.ConfigureAwait(false)).MatchAsync(onSuccess, onFailure, ct)
+            .ConfigureAwait(false);
 }
